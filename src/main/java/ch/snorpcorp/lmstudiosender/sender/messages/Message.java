18458d5af03abfa56d260e9fa69f3e7f4e1e7c13
Liam Mihm <bmihml@bbcag.ch>
2026-08-16 20:47:18 +0200

    Added Messages

---
+++ src/main/java/ch/snorpcorp/lmstudiosender/sender/messages/Message.java
@@ -0,0 +1,33 @@
+﻿package ch.snorpcorp.lmstudiosender.sender.messages;
+
+import java.io.Serializable;
+
+public class Message {
+    private MessageRoles role;
+    private String message;
+
+    public Message (MessageRoles role, String message) {
+        this.role = role;
+        this.message = message;
+    }
+
+    public String getRoleString() {
+        return role.toString().toLowerCase();
+    }
+
+    public MessageRoles getRole() {
+        return role;
+    }
+
+    public void setRole(MessageRoles role) {
+        this.role = role;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/ch/snorpcorp/lmstudiosender/sender/messages/MessageRoles.java
@@ -0,0 +1,7 @@
+﻿package ch.snorpcorp.lmstudiosender.sender.messages;
+
+public enum MessageRoles {
+    SYSTEM,
+    USER,
+    ASSISTANT,
+}
